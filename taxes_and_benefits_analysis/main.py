804d17f85d04736ed37f5256f67ab91467f44909
koushik006 <66548270+koushik006@users.noreply.github.com>
2025-04-22 00:15:23 +0100

    Update main.py
    changing the file path

--- taxes_and_benefits_analysis/main.py
+++ taxes_and_benefits_analysis/main.py
@@ -23,9 +23,9 @@ from data_visualization import (
 
 def main():
     # File paths
-    tax_input_file = '/content/drive/MyDrive/Data/tax-benefits-statistics-time-series-v3-filtered-2025-03-17T15-44-252.xlsx'
-    unemp_input_file = '/content/drive/MyDrive/Data/Copy of Unemployment_rate_monthly.csv'
-    infl_input_file = '/content/drive/MyDrive/Data/Copy of Inflation_monthly.csv'
+    tax_input_file = 'csv/tax-benefits-statistics-time-series-v3-filtered-2025-03-17T15-44-25Z_data.xlsx
+    unemp_input_file = 'csv/Inflation_monthly_data.csv'
+    infl_input_file = 'csv/Inflation_monthly_data.csv'
     tax_output_csv = 'cleaned_tax_benefits_data.csv'
     econ_output_csv = 'cleaned_economic_indicators_data.csv'
     
